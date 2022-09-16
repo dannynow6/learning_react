f6ffc10a9e363175ea8d7e8573f78d1adeff28f4
Dan Garro <dobetterwithdan@gmail.com>
2022-09-16 19:24:15 -0400

    fetched data and added image from github avatar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
-function GithubUser ({ name, location, pRepos, bio }) {
+function GithubUser ({ name, location, pRepos, bio, avatar }) {
   return (
     <div>
       <h1>{name}</h1>
+      <img height={150} src={avatar} alt={name}/>
       <h5>{bio}</h5>
       <p>Located in {location}</p>
       <p>Number of Public Repos: {pRepos}</p>
@@ -18,7 +19,7 @@ function App() {
     fetch(`https://api.github.com/users/dannynow6`).then((response) => response.json()).then(setData);
   }, []);
   if(data) 
-    return <GithubUser name={data.name} location={data.location} pRepos={data.public_repos} bio={data.bio}/>;
+    return <GithubUser name={data.name} location={data.location} pRepos={data.public_repos} bio={data.bio} avatar={data.avatar_url}/>;
   return <h1>Data</h1>;
 }
 
