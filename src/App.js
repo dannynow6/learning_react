cdd4545b5f616852f0a4d28b26c7ad375aa9f91e
Dan Garro <dobetterwithdan@gmail.com>
2022-09-17 12:30:00 -0400

    displaying list data

--- src/App.js
+++ src/App.js
@@ -1,46 +1,22 @@
+import React from "react";
 import "./App.css";
-import { useState, useEffect } from "react";
 
-function GithubUser ({ name, location, pRepos, bio, avatar }) {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <img height={150} src={avatar} alt={name}/>
-      <h5>{bio}</h5>
-      <p>Located in {location}</p>
-      <p>Number of Public Repos: {pRepos}</p>
-    </div>
-  );
+const tahoe_peaks = [
+  { name: "Freel", elevation: 10891},
+  { name: "Monument", elevation: 10067},
+  { name: "Danny's", elevation: 14982},
+  { name: "Mo's", elevation: 34432},
+  { name: "Tallac", elevation: 9735}
+];
+
+function List({data, renderItem, renderEmpty}) {
+  return !data.length ? renderEmpty : (<ul>{data.map((item) => (<li key={item.name}>{renderItem(item)}</li>))}</ul>);
 }
 
 function App() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    fetch(
-      `https://api.github.com/users/dannynow6`
-      )
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, []);
-    
-  if (loading) return <h1>Loading...</h1>;
-  if (error) 
-    return <pre>{JSON.stringify(error)}</pre>;
-  if (!data) return null;
+  
   return (
-    <GithubUser 
-      name={data.name} 
-      location={data.location} 
-      pRepos={data.public_repos} 
-      bio={data.bio} 
-      avatar={data.avatar_url} 
-    />
+    <List data={tahoe_peaks} renderEmpty={<p>This list is empty...</p>} renderItem={item => <>{item.name} - {item.elevation} ft.</>}/>
   );
     
 }
